c2654c4c80a9c096c6dc3c7044b99d25c9d4af8a
Dariusz Birk <109848318+DB-Byd@users.noreply.github.com>
2022-08-17 14:35:04 +0200

    Add files via upload

---
+++ Pyt_zad_220816_01_02.py
@@ -0,0 +1,56 @@
+print("\n*** Łączenie, sortowanie i odwracanie list: ***\n")
+
+l1 = [11, 8, 7, 10, 6, 9]
+print("Lista l1: ", l1)
+l2 = [1, 3, 2, 4, 5]
+print("Lista l2: ", l2)
+l3 = l1 + l2
+print("Połączone listy l1 i l2 - lista l3:", l3)
+
+l3.sort()
+print("Posortowana lista l3:", l3, "\n")
+
+print("Zadanie 1, odwracanie list.\n")
+# https://datagy.io/python-reverse-list/
+
+reversed_01 = l3
+reversed_01.reverse()
+print("Lista l3 odwrócona 1. sposobem:", reversed_01)
+
+reversed_02 = list(reversed(l3))
+print("Lista l3 odwrócona 2. sposobem:", reversed_02)
+
+reversed_03 = reversed_02[::-1]
+print ("Lista l3 odwrócona 3. sposobem:", reversed_03)
+
+slicer = slice(None, None, -1)
+reversed_04 = reversed_03[slicer]
+print ("Lista l3 odwrócona 4. sposobem:", reversed_04)
+
+reversed_05 = []
+for item in reversed(reversed_04):
+    reversed_05.append(item)
+print ("Lista l3 odwrócona 5. sposobem:", reversed_05)
+
+reversed_06 = [reversed_05[i] for i in range(len(reversed_05) - 1, -1, -1)]
+print ("Lista l3 odwrócona 6. sposobem:", reversed_06)
+
+
+print("\nZadanie 2, sprawdzanie obecności elementu na liście.\n")
+print("Zabawa w sprawdzanie obecności liczby na liście:")
+
+while True:
+    print('\nAby zakończyć naciśnij "q"')
+    liczba = input("Podaj liczbę: ")
+    if liczba == "q":
+        break
+    else:
+        if int(liczba) in reversed_06:
+            check = True
+        else:
+            check = False
+        print(check)    
+print("\n**** Dzięki za klikanie :))) ****\n\n")
+
+
+
---
+++ Pyt_zad_220817_04.py
@@ -0,0 +1,148 @@
+# Przed analizą kodu proszę uruchomić program - będzie lepsza zabawa :)
+# Taką mam nadzieję!
+
+# Warunki zadania:
+
+# Definiowanie listy l1
+l1 = ["K", "A", "U", "B", "R", "E", "W", "T", "A", "R", " ", "O", "A", "K", "W", "O", "R", "I", "U", "A", "K", "O"]
+# print("Lista l1:", l1, "o długości:", len(l1))
+
+# Definiowanie listy l2
+l2 = ["!", "R", "A", "!", "P", "Q", "!", "G", "A", "Ć", "F", "B", "A", "Q", "G", "M"]
+# print("Lista l2:", l2, "o długości:", len(l2))
+
+# Przetwarzanie drogi l1:
+# Identyfikowanie indeksu elementu znajdującego się w połowie listy:
+l1_ind_mid = len(l1)//2
+# print(l1_ind_mid)
+# Tworzenie nowej listy z wycinka listy l1 i co drugiego elementu:
+l1_wyc = l1[:l1_ind_mid:2]
+# print(l1_wyc)
+
+# Przetwarzanie drogi l2:
+# Odwrócenie listy l2:
+l2.reverse()
+# print(l2)
+# Tworzenie nowej listy z co trzeciego elementu:
+l2_wyc = l2[::3]
+# print(l2_wyc)
+
+# Tworzenie drogi z połączenia przetworzonych list:
+l3_haslo = l1_wyc + l2_wyc
+# print(l3_haslo)
+
+# Dalej idzie zabawa w programowanie:
+
+# Zamierzam wyświetlić drogę (w tym przypadku hasło), zatem trzeba przekształcić 
+# listę w string.
+# https://www.simplilearn.com/tutorials/python-tutorial/list-to-string-in-python
+
+haslo = ' '.join(l3_haslo)
+# print(haslo)
+
+# Teraz będą wyświetlane komunikaty, ale sukcesywnie, z pewnym opóźnieniem.
+# https://www.pythoncentral.io/pythons-time-sleep-pause-wait-sleep-stop-your-code
+import time
+
+print("\n\nZadanie 4 - Droga ucieczki inaczej! :)")
+time.sleep(2)
+
+print('"Seksmisja", scena w windzie.\n')
+time.sleep(2)
+
+print('Narrator:\nMax i Albert uciekają. Pomaga im Lamia.\nNiestety winda nie chce ruszyć...')
+time.sleep(2)
+
+print("\nLamia:")
+time.sleep(1)
+print("- Dlaczego właz nie zjeżdża w dół?")
+time.sleep(1)
+print("- Co jest? Co się stało? Dlaczego to nie zjeżdża!")
+time.sleep(1)
+print("- Pewnie dały nie ten klucz!")
+time.sleep(2)
+
+print("\nStrażniczka:")
+time.sleep(1)
+print("- Klucz nie wystarcza. Potrzebne akustyczne hasło.")
+time.sleep(2)
+
+print("\nLamia:")
+time.sleep(1)
+print("- Jakie hasło?")
+time.sleep(2)
+
+print("\nStrażniczka:")
+time.sleep(1)
+print("- Nie mamy pojęcia!")
+time.sleep(1)
+print("- Hasło zna tylko Jej Ekscelencja!")
+time.sleep(2)
+
+print("\nAlbert:")
+time.sleep(1)
+print("- Dlaczego nas nie chcą wypuścić, żebyśmy tam sobie spokojnie umarli...")
+time.sleep(2)
+
+print("\nMaks:")
+time.sleep(1)
+print("- Wszystko przepadło...")
+time.sleep(2)
+
+print("-", haslo)
+time.sleep(1)
+
+print("\nNarrator:")
+time.sleep(1)
+print("O rety!")
+time.sleep(1)
+print("Wywalą programistę z tego harcerstwa! :)")
+time.sleep(1)
+print("Trzeba ratować sytuację: ")
+time.sleep(1)
+print("- To nie on!!! To taki scenariusz!!! ;)")
+time.sleep(2)
+print("\nA tymczasem:")
+time.sleep(2)
+print("WINDA ZACZYNA ZJEŻDŻAĆ!\n\n")
+time.sleep(2)
+
+# Wyjaśnienie zadania dla widzów
+
+print("Co takiego wykombinował Maks?\n")
+time.sleep(1)
+print("Otóż w kaloszu, w siedlisku Dekadencji, znalazł dwie listy:\n")
+time.sleep(1)
+print("Listę l1:\n", l1)
+time.sleep(1)
+print("\ni listę l2:\n", l2)
+time.sleep(1)
+print('\nA w butelce "nasi" zostawili mu algorytm:\n')
+time.sleep(1)
+print("- Podziel listę l1 na pół i z pierwszej połowy weź co drugi element.")
+time.sleep(1)
+print("\nWyszła taka lista:\n", l1_wyc)
+time.sleep(1)
+print("\n- Drugą listę odwróć i wybierz z niej co trzeci element." )
+time.sleep(1)
+print("\nWyszło coś takiego:\n", l2_wyc )
+time.sleep(1)
+print("\n- Następnie połącz listy i otrzymasz tajne hasło.")
+time.sleep(1)
+print("\nWyszła taka lista z hasłem:\n", l3_haslo)
+time.sleep(2)
+print("\nA teraz najważniejsze pytanie:")
+time.sleep(2)
+print("\nStrażniczka:")
+time.sleep(1)
+print("- A co to znaczy?")
+time.sleep(2)
+
+print("\n\nWrażliwych oczywiście przepraszam!\nNa co dzień tak nie szaleję! ;)")
+time.sleep(2)
+print("\nBye!!! :))))")
+time.sleep(2)
+print("\nTHE END\n\n\n\n")
+time.sleep(3)
+
+
